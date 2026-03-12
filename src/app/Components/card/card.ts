dcf893d11a02bda79b252673639b54dc6de8b32a
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2026-03-12 14:15:22 -0300

    Correção de ciclo de vida do card usando signals

--- src/app/Components/card/card.ts
+++ src/app/Components/card/card.ts
@@ -1,5 +1,6 @@
-import { Component, effect, input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component, DestroyRef, effect, inject, input, OnChanges, OnInit, signal, SimpleChanges } from '@angular/core';
 import { Pokemon } from '../../Services/pokemon';
+import { catchError, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-card',
@@ -7,54 +8,59 @@ import { Pokemon } from '../../Services/pokemon';
   templateUrl: './card.html',
   styleUrl: './card.scss',
 })
-export class Card implements OnInit{
-  nome = input.required<string>()
-  type1:string = ''
-  type2: string = ''
-  sprite: string= ''
-  loading = true
+export class Card{
   
-  constructor(private pokemonService:Pokemon){}
-  
-  ngOnInit(): void {
-    const nome = this.nome()
+  nome = input.required<string>();
 
-    if (!nome) {
-      console.warn('Nome inválido recebido pelo Card');
-      this.loading = false;
-      return;
-    }
+  type1 = signal<string>('');
+  type2 = signal<string>('');
+  sprite = signal<string>('');
+  loading = signal<boolean>(false);
+  error = signal<string | null>(null);
 
-    this.pokemonService.getPokemonByName(this.nome()).subscribe({
-      next: (pkm:any) =>{
-        this.type1 = pkm.types[0].type.name
-        this.type2 = pkm.types[1]?.type?.name || ''
-        this.sprite = pkm.sprites.front_default
-        this.loading = false
-      },
-      error: (error) =>{
-        console.log(error)
-        this.loading = false
+  private svc = inject(Pokemon);
+  private destroyRef = inject(DestroyRef);
+  private sub?: Subscription;
+
+  constructor() {
+    effect(() => {
+      const n = this.nome();
+
+      this.error.set(null);
+      this.type1.set('');
+      this.type2.set('');
+      this.sprite.set('');
+
+      this.sub?.unsubscribe();
+
+      if (!n) {
+        this.loading.set(false);
+        return;
       }
-    })
-  }
 
-  // ngOnChanges(changes: SimpleChanges) {
-  //   if (changes['nome'] && changes['nome'].currentValue) {
-  //     this.loading = true;
-  //     this.pokemonService.getPokemonByName(this.nome()).subscribe({
-  //       next: (pkm:any) =>{
-  //         this.type1 = pkm.types[0].type.name
-  //         this.type2 = pkm.types[1]?.type?.name || ''
-  //         this.sprite = pkm.sprites.front_default
-  //         this.loading = false
-  //       },
-  //       error: (error) =>{
-  //         console.log(error)
-  //         this.loading = false
-  //       }
-  //     });
-  //   }
-  // }
+      this.loading.set(true);
+
+      this.sub = this.svc
+        .getPokemonByName(n)
+        .pipe(
+          catchError((err) => {
+            console.error('Erro ao carregar pokemon:', err);
+            this.error.set('Erro ao carregar dados.');
+            return of(null);
+          })
+        )
+        .subscribe((pkm: any) => {
+          if (pkm) {
+            this.type1.set(pkm?.types?.[0]?.type?.name ?? 'desconhecido');
+            this.type2.set(pkm?.types?.[1]?.type?.name ?? '');
+            this.sprite.set(pkm?.sprites?.front_default ?? '');
+          }
+          this.loading.set(false);
+        });
+      }
+    );
+
+    this.destroyRef.onDestroy(() => this.sub?.unsubscribe());
+  }
 
 }
